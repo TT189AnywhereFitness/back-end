1c8f1a148a44a11351edfe80a505bc749b427571
Lukecp <lukecp@pm.me>
2021-04-22 22:47:15 -0400

    created db model functions

--- api/classes/class-model.js
+++ api/classes/class-model.js
@@ -1,1 +1,32 @@
-// to retrive class info from DB
+const db = require('../../data/db-config.js');
+
+const get = () => {
+	return db('classes as c')
+		.join('users as u', 'u.id', 'c.user_id')
+		.select('*')
+}
+
+const findById = (id) => {
+	return db('classes as c')
+		.join('users as u', 'u.id', 'c.user_id')
+		.where({ id })
+		.first();
+}
+
+const add = async (newClass) => {
+	const id = await db('classes').insert(newClass);
+
+	return findById(id);
+}
+
+const update = async (id, updatedClass) => {
+	const id = await db('classes').where({ id }).update(updatedClass)
+
+	return findById(id);
+}
+
+module.exports = {
+	get,
+	findById,
+	add
+}
