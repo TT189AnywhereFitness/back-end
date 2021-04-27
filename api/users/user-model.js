e68f643b1071cfe243c3d1efdd335ee7de0ed1f3
AlexAndrew Mallory <pytormal@live.com>
2021-04-26 20:24:27 -0700

    added user-model data, not tested

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -1,1 +1,38 @@
 // to interact with the user DB from auth router & middleware
+const db = require('../../data/db-config')
+const { getById } = require('../classes/class-model')
+
+function get() {
+    return db('users').select('user_name','role')
+}
+
+function getBy(filter) {
+    return db('users').where(filter).orderBy('user_name')
+}
+
+async function add(newUser) {
+    const [user_id] = await db('users').instert(newUser, 'user_id')
+
+    return getById(user_id)
+}
+
+function getById(user_id) {
+    return db("users").select("user_name", "role", 'password').where({'user_id':user_id})
+}
+
+function update(user_id, newdata) {
+  return db("users").update(newdata).where({ user_id });
+}
+
+function remove(id) {
+  return db("users").del().where({ user_id });
+}
+
+module.exports = {
+    get,
+    getBy,
+    add,
+    getById,
+    update,
+    remove
+};
