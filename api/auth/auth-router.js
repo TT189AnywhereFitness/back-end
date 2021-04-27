9a423adbf36294f6286ab37d27475e206b2d12a7
Lukecp <lukecp@pm.me>
2021-04-26 23:28:27 -0400

    Structured auth endpoints

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,16 +2,25 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../users/user-model.js');
 
+
+//POST Register a new user
 router.post('/register', (req, res, next) => {
-
+	const newUser = req.body;
+	
+	Users.add(newUser)
+		.then((createdUser) => res.status(200).json(createdUser))
+		.catch(err => next({...err, status: 500}));
 });
 
+
+//POST login an existing user
 router.post('/login', (req, res, next) => {
-  
+  const {username, password} = req.body;
+
+	Users.findBy({username})
+		.then((foundUser) => res.status(200).json(foundUser))
+		.catch(err => next({...err, status: 500}));
 });
 
-router.post('/logout', (req, res, next) => {
-  
-});
 
 module.exports = router;
