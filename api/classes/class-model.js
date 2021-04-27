26e66680d6acd8d2411db81b0b1f0194e296578e
Lukecp <lukecp@pm.me>
2021-04-26 22:11:41 -0400

    updated class model to match table names

--- api/classes/class-model.js
+++ api/classes/class-model.js
@@ -2,14 +2,14 @@ const db = require('../../data/db-config.js');
 
 const get = () => {
 	return db('classes as c')
-		.join('users as u', 'u.id', 'c.user_id')
+		.join('users as u', 'u.user_id', 'c.instructor_id')
 		.select('*')
 }
 
 const getById = (id) => {
 	return db('classes as c')
-		.join('users as u', 'u.id', 'c.user_id')
-		.where({ id })
+		.join('users as u', 'u.user_id', 'c.instructor_id')
+		.where({ 'class_id': id })
 		.first();
 }
 
@@ -20,9 +20,9 @@ const add = async (newClass) => {
 }
 
 const update = async (id, updatedClass) => {
-	const updatedId = await db('classes').where({ id: updatedId }).update(updatedClass)
+	await db('classes').where({ 'class_id': id }).update(updatedClass)
 
-	return getById(updatedId);
+	return getById(id);
 }
 
 module.exports = {
