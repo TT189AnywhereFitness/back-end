db836920142fc453062068b1ff6bf50aad3782b7
AlexAndrew Mallory <pytormal@live.com>
2021-04-29 18:58:26 -0700

    added a validate password, untested

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,15 +1,33 @@
 // for restricted, credential validation, etc.
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
 
 const checkUsernameAvail = (req, res, next) => {
 	next();
 };
 
 const validatePassword = (req, res, next) => {
-	next();
+	try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (token) {
+      jwt.verify(token, secret, (err, decodedToken) => {
+        if (err) {
+          next({ apiCode: 401, apiMessage: "invalid or missing credentials" });
+        } else {
+          req.decodedToken = decodedToken;
+          next();
+        }
+      });
+    } else {
+      next({ apiCode: 401, apiMessage: "invalid or missing credentials" });
+    }
+  } catch (err) {}
+  next({ apiCode: 500, apiMessage: "error validating credentials", ...err });
 };
 
 const checkUserExists = (req, res, next) => {
-	next();
+  next({ apiCode: 500, apiMessage: 'error validating credentials', ...err });
 };
 
 module.exports = {
